0d13eaab108c81164fabbc3870bd5d033f4d3ff2
hasanfaridi <saharayane_co@yahoo.com>
2022-07-27 19:27:03 +0430

    commit font New.txt

---
+++ WebApplication1/DTOs/Home/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.DTOs.Home
+{
+  public  class LoginViewModel
+    {
+        public string CustomerMobile { get; set; }
+        public string CustomerPassword { get; set; }
+        public bool RememberMe { get; set; }
+
+    }
+}
